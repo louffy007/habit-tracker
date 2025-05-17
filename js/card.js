b33c64b2e02fccbde43d0d0253318d3e10814d5f
louffy007 <mouad.tebbaai7@gmail.com>
2025-05-17 16:27:52 +0100

    adding cards and style them and othe small changes

---
+++ js/card.js
@@ -0,0 +1,61 @@
+// ==========================
+// * HABIT RENDERING
+// ==========================
+
+//creat one card.
+function elementsAdding(habit) {
+  let sectionContainer = document.querySelector(".section-container");
+  //creat card elements
+  let habitCardElement = document.createElement("article");
+  let cardTop = document.createElement("div");
+  let habitTitleElement = document.createElement("h4");
+  let habitDescElement = document.createElement("p");
+  let habitCatElement = document.createElement("p");
+
+  //create card buttons elements
+  let buttonsContainer = document.createElement("div");
+  let habitDeleteElement = document.createElement("button");
+  let habitEditElement = document.createElement("button");
+  let deleteIcon = document.createElement("img");
+  let editIcon = document.createElement("img");
+
+  //add classess to the elements
+  habitCardElement.classList.add("card", "radius-md");
+  cardTop.classList.add("card-top");
+  habitCatElement.classList.add("card-cat", "radius-md");
+  habitDescElement.classList.add("card-description");
+
+  //add clasess and srcs to the card buttons
+  buttonsContainer.classList.add("buttons-container");
+  habitDeleteElement.classList.add("delete-button");
+  habitEditElement.classList.add("delete-button");
+  deleteIcon.classList.add("delete-icon");
+  editIcon.classList.add("edit-icon");
+  deleteIcon.setAttribute("src", "assets/icons/trash.svg");
+  editIcon.setAttribute("src", "assets/icons/pencil-square.svg");
+
+  // adding content dynamicly to the card
+  habitTitleElement.textContent = habit.title;
+  habitDescElement.textContent = habit.description;
+  habitCatElement.textContent = habit.category;
+
+  // appending elements
+  cardTop.appendChild(habitTitleElement);
+  cardTop.appendChild(habitCatElement);
+  habitCardElement.appendChild(cardTop);
+  habitCardElement.appendChild(habitDescElement);
+
+  habitDeleteElement.appendChild(deleteIcon);
+  habitEditElement.appendChild(editIcon);
+  buttonsContainer.appendChild(habitDeleteElement);
+  buttonsContainer.appendChild(habitEditElement);
+  habitCardElement.appendChild(buttonsContainer);
+
+  sectionContainer.appendChild(habitCardElement);
+}
+// loop through array and call the one card function
+function cardRendering(habitArr) {
+  for (let i = 0; i < habitArr.length; i++) {
+    elementsAdding(habitArr[i]);
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,14 +1,43 @@
-let nameFiled = document.querySelector(".name span");
-let goalField = document.querySelector(".goal span");
-let addHabit = document.querySelector(".add-habit");
-let modalDiv = document.querySelector(".modal");
+// ==========================
+// * USER INFO + HEADER DISPLAY
+// ==========================
 
+// header info and button.
+let nameField = document.querySelector(".name span");
+let goalField = document.querySelector(".goal span");
+
+// we get the current user info fro mthe storage.
 let getUser = window.localStorage.getItem("currentUser");
 let currentUser = JSON.parse(getUser);
+if (currentUser == null) {
+  window.location.href = "signup.html";
+}
 
-nameFiled.textContent = currentUser.name;
+nameField.textContent = currentUser.name;
 goalField.textContent = currentUser.goal;
 
+// ==========================
+// * STORAGE SETUP & HABIT ARRAY
+// ==========================
+
+//parse and get item and pass them to the array
+let habits =
+  JSON.parse(localStorage.getItem(`user: ${currentUser.email}`)) || [];
+
+window.onload = function () {
+  cardRendering(habits);
+};
+
+
+
+// ==========================
+// * MODAL: OPEN / CLOSE HANDLING
+// ==========================
+//add habit button
+let addHabit = document.querySelector(".add-habit");
+//modal.
+let modalDiv = document.querySelector(".modal");
+// open and close the modal box.
 addHabit.onclick = function () {
   modalDiv.classList.add("open");
 };
@@ -17,3 +46,65 @@ modalDiv.onclick = function (e) {
     modalDiv.classList.remove("open");
   }
 };
+// ==========================
+// *  HABIT FORM: INPUT, VALIDATION, SUBMISSION
+// ==========================
+// geting the modal form info.
+let habitTitle = document.querySelector("#habit-title");
+let habitCat = document.querySelector("#habit-cat");
+let habitDesc = document.querySelector("#habit-description");
+let habitSubmit = document.querySelector(".submit-btn");
+
+// getting the form data
+function getFormData() {
+  let dataObject = {
+    id : Date.now(),
+    title: habitTitle.value,
+    category: habitCat.value,
+    description: habitDesc.value,
+  };
+  return dataObject;
+}
+// validate the data
+function validateData(data) {
+  if (data.title != "" && data.category != "" && data.description != "") {
+    return true;
+  } else {
+    return false;
+  }
+}
+//add habits to array and storage
+function createAndPushHabits(object, email) {
+  habits.push(object);
+
+  localStorage.setItem(`user: ${email}`, JSON.stringify(habits));
+}
+//empty the data
+function clearDataInput() {
+  document.forms[0].reset();
+}
+
+//warning
+let warningDiv = document.createElement("div");
+warningDiv.className = "warning-message py-2 radius-md";
+
+//habit submit function
+
+let handleSubmit = function (e) {
+  e.preventDefault();
+  let habitData = getFormData();
+  let validHabitData = validateData(habitData);
+
+  if (validHabitData) {
+    createAndPushHabits(habitData, currentUser.email);
+    elementsAdding(habitData);
+    clearDataInput();
+    modalDiv.classList.remove("open");
+  } else {
+    if (!document.forms[0].contains(warningDiv)) {
+      warningDiv.textContent = "Your Inputs are empty enter a valid data!";
+      document.forms[0].appendChild(warningDiv);
+    }
+  }
+};
+habitSubmit.onclick = handleSubmit;
